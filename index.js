153f51980f129089ba1513a9a46bd6968caaffff
dhritiman78 <dhritiman.saikia.11b.244@gmail.com>
2024-11-07 01:19:05 +0530

    modified register controller and added login contrller

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const connectDB = require('./utils/db')
+const cors = require('cors')
 const app = express()
 const authRoutes = require('./routes/auth')
 
 app.use(express.json());
+
+// Tackling the CORS
+app.use(cors({
+    origin: '*', // Update this to match your actual frontend domain
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+// Handling preflight requests
+app.options('/api/contact', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.send();
+});
 
 connectDB()
 
